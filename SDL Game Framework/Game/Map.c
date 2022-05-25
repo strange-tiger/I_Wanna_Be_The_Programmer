e24ac34b61d2cc5cef47efdee8e098e46ef348af
csy-59 <csy010509@naver.com>
2022-05-25 20:13:51 +0900

    Upadate: 헤더 파일 변수 부분 완성 Scene.c 추가바람_0525_csy

---
+++ SDL Game Framework/Game/Map.c
@@ -0,0 +1,19 @@
+#include "Map.h"
+
+void Map_Init(Map* map, int32 StageNum) {
+	//csv 파일에서 파싱해오기. (추후 csv 헤더 확정 후 수정 예정
+	CsvFile csv;
+
+}
+
+void Map_Update(Map* map) {
+	
+}
+
+void Map_Render(Map* map) {
+
+}
+
+void Map_Release(Map* map) {
+
+}
