6e810ec9f32f8b0479fdbb1ec8eca828db48c6b7
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-05-26 10:52:24 +0900

    22.05.26_10:52 Commit : Update Image File
    이미지 파일 업로드(크레딧, 타이틀)

--- SDL Game Framework/Framework/Scene.c
+++ SDL Game Framework/Framework/Scene.c
@@ -12,12 +12,22 @@ static ESceneType s_nextScene = SCENE_NULL;
 typedef struct TitleSceneData
 {
 	//배경
-	Image	BGImage;
-	Music	BGM;
+	Image	Title_BackGroundImage;
+	Image	Title_URL;
+	Image	Title_SmallTitle;
+	int32		Title_SmallTitle_posY;
+	Image	Title_BigTitle;
+	int32		Title_BigTitle_posY;
+	Image	Title_PressStart;
+	Image	Title_GameStart;
+	Image	Title_Credit;
+	Image	Title_Exit;
+	Image	Title_Arrow;
+	int32		Title_Arrow_posY;
 
-	//그냥 넣은 텍스트 삭제하셔도 됩니당
-	Text	mainText;
-	Text	NextText;
+	float	TitleScenePlayTime;
+
+	Music	BGM;
 } TitleSceneData;
 
 void init_title(void)
@@ -26,43 +36,124 @@ void init_title(void)
 	memset(g_Scene.Data, 0, sizeof(TitleSceneData));
 
 	TitleSceneData* data = (TitleSceneData*)g_Scene.Data;
+	
 
-	Text_CreateText(&data->mainText, "d2coding.ttf", 40,L"이거슨 타이틀", wcslen(L"이거슨 타이틀"));
-	Text_CreateText(&data->NextText, "d2coding.ttf", 40,L"Space 눌러 다음 씬으로", wcslen(L"Space 눌러 다음 씬으로"));
+	//Image File Load
+	Image_LoadImage(&data->Title_BackGroundImage, "TitleImage01_BackGroundImage.png");
+	Image_LoadImage(&data->Title_URL, ".png");
+	Image_LoadImage(&data->Title_SmallTitle, ".png");
+	Image_LoadImage(&data->Title_BigTitle, ".png");
+	Image_LoadImage(&data->Title_PressStart, ".png");
+	Image_LoadImage(&data->Title_GameStart, ".png");
+	Image_LoadImage(&data->Title_Credit, ".png");
+	Image_LoadImage(&data->Title_Exit, ".png");
+	Image_LoadImage(&data->Title_Arrow, ".png");
+
+
+	data->Title_SmallTitle_posY = -600;
+	data->Title_BigTitle_posY = -400
+
+
+
+
+	//블랙스크린으로 시작
+	//로고 페이드 인(1초) - 유지(3초) - 아웃(1초)
+	//애쉬 배경 출력 페이드인(1초)
+	//메인타이틀 위에서 아래로Y++; (0.5초)
+	//메인타이틀 줌 인 - 아웃 반복(스케일 조정)
+	//선택지 선택시마다 강조 효과
+	
+	
+	/*
+	선택지
+	Start
+	Credit
+	Exit
+	*/
+
+
+	/*
+	Control
+	VK_UP
+	VK_DOWN
+	VK_ENTER
+	*/
+
+
 }
 
 void update_title(void)
 {
+	TitleSceneData* data = (TitleSceneData*)g_Scene.Data;
+	data->TitleScenePlayTime += Timer_GetDeltaTime();
+
+	if (data->TitleScenePlayTime < 2)
+	{
+		Image_SetAlphaValue(0, &data->LogoForBlackScreen - 255, 255);
+	}
+	if (2 <= data->TitleScenePlayTime && data->TitleScenePlayTime < 3)
+	{
+		Image_SetAlhpaValue(0, &data->LogoForBlackScreen + 10, 255);
+	}
+	if (5 <= data->TitleScenePlayTime && data->TitleScenePlayTime < 6)
+	{
+		Image_SetAlhpaValue(0, &data->LogoForBlackScreen - 10, 255);
+	}
+
+
+	//Input Key in Title Scene 
+	if (Input_GetKeyDown(VK_UP) && data->ValueForSelectMenu > 1 )
+	{
+		data->ValueForSelectMenu--;
+	}
+	if (Input_GetKeyDown(VK_DOWN) && data->ValueForSelectMenu < 3)
+	{
+		data->ValueForSelectMenu++;
+	}
+
 	if (Input_GetKeyDown(VK_SPACE))
 	{
-		Scene_SetNextScene(SCENE_MAIN);
+		switch (data->ValueForSelectMenu)
+		{
+		case 1:
+			Scene_SetNextScene(SCENE_MAIN);
+			break;
+		case 2:
+			Scene_SetNextScene(SCENE_CREDIT);
+			break;
+		case 3:
+			//Exit															//EXIT 구현 요. 선택시 한번 더 확인할건지??그냥 끌건지.
+			break;
+		}
 	}
 }
 
 void render_title(void)
 {
-	g_Scene.Data = malloc(sizeof(TitleSceneData));
-	memset(g_Scene.Data, 0, sizeof(TitleSceneData));
-
 	TitleSceneData* data = (TitleSceneData*)g_Scene.Data;
 
-	SDL_Color bgColor = { 0, 0, 0, 255 };
-	SDL_Color textColor = { 255, 255, 255, 255 };
-	Renderer_DrawTextShaded(&data->mainText, 0, 0, bgColor, textColor);
-	Renderer_DrawTextShaded(&data->NextText, 0, 0, bgColor, textColor);
+	Renderer_DrawImage(&data->Title_BackGroundImage, 0, 0);
+	Renderer_DrawImage(&data->Title_BigTitle, 590, );
+	
+	//Position_X, Y 수정 요.
+	if (data->TitleScenePlayTime >= 6)
+	{
+		Renderer_DrawImage(&data->BGImage, 0, 0);
+		Renderer_DrawImage(&data->MainTitle, 0, 0);
+		Renderer_DrawImage(&data->GameStart, 0, 0);
+		Renderer_DrawImage(&data->Credit, 0, 0);
+		Renderer_DrawImage(&data->Exit, 0, 0);
+	}
+
+
 }
 
 void release_title(void)
 {
-	g_Scene.Data = malloc(sizeof(TitleSceneData));
-	memset(g_Scene.Data, 0, sizeof(TitleSceneData));
-
 	TitleSceneData* data = (TitleSceneData*)g_Scene.Data;
 
-	Text_FreeText(&data->mainText);
-	Text_FreeText(&data->NextText);
 
-	SafeFree(data); 
+	SafeFree(data);
 }
 #pragma endregion
 
@@ -112,7 +203,7 @@ void release_main(void)
 #pragma endregion
 
 #pragma region CreditScene
-//추가 바람
+//추가 바람 <<ㅇㅋ
 
 
 #pragma endregion
